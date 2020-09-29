36c36d50bcfff02cabe98612c3918089c6cf9d97
Oliver Beach <17b23772@marling.school>
2020-09-29 10:20:45 +0000

    nearly finished

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 #Introduces the joke game
 import time
-score = 0
-print ("Welcome to the joke game")
-time.sleep(3)
-print ("Here is the 1st Joke!")
-#\n shows a New Line
-print("\n")
-time.sleep(2)
-print ("What is pink and fluffy")
-a = str(input())
-if a == str("Pink Fluff"):
-  print ("You are Correct")
-  score = score + 1
-else:
-  print ("")
-print ("\n")
+def jokes() :
+  score = 0
+  print ("Welcome to the joke game")
+  time.sleep(3)
+  print ("Here is the First Joke!")
+  #\n shows a New Line
+  print("\n")
+  time.sleep(2)
+  print ("What is pink and fluffy")
+  a = str(input())
+  if a == str("Pink Fluff"):
+    print ("You are Correct")
+    score = score + 1
+    time.sleep(2)
+    print(score)
+  else :
+    print ("You are wrong")
+    time.sleep(2)
+    score = score -1
+    print(score)
+    
+if 1 == 1 jokes()
+
+
+
+
